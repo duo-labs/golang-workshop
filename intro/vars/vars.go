fa8e4d5120bb5125a2bbf6c19d3ca8dbac893a60
Jordan Wright <jwright@duosecurity.com>
2019-07-24 21:45:07 -0400

    Added intro files

---
+++ intro/vars/vars.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. TODO Basic assignment
+	// Make a string with your name and print it out
+	
+
+	// 2. TODO Assigning multiple things
+	// Assign strings for your family members (modify below)
+	var father, sister = "PoolDad", "Gina"
+	fmt.Printf("%s and %s\n", father, sister)
+
+
+	// 3. bools and ints!
+	var is_awesome = true
+	var age = 42
+	fmt.Println(is_awesome, age)
+	
+	
+	// 4. TODO Make a map of whatever you want, call it best_map
+	// Try to include ints or bools!
+	// HINT: make(map[string]int)
+	
+	// fmt.Println("map:", best_map)
+}
